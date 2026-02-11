c647f69c427e84f5b55f2bc1176f9d5014b7bbfa
t66587979-dotcom <t66587979@gmail.com>
2026-02-11 21:13:48 +0600

    Update baby.js

--- modules/commands/baby.js
+++ modules/commands/baby.js
@@ -45,7 +45,7 @@ module.exports.config = {
  hasPermssion: 0,
  credits: "rX",
  description: "AI auto teach with Teach & List support + Typing effect",
- commandCategory: "Utility",
+ commandCategory: "System",
  usages: "[query]",
  cooldowns: 0,
  prefix: false
@@ -179,7 +179,7 @@ module.exports.handleEvent = async function ({ api, event, Users }) {
  if (!text || !simsim) return;
 
  const senderName = await Users.getNameUser(event.senderID);
- const triggers = ["baby", "bby", "bot", "bbz", "toru", "kakashi"];
+ const triggers = ["baby", "bby", "xan", "toru", "bot", "kakashi"];
 
  if (triggers.includes(text)) {
  const replies = [
@@ -216,7 +216,7 @@ module.exports.handleEvent = async function ({ api, event, Users }) {
  });
  }
 
- const matchPrefix = /^(baby|bby|bot|bbz|toru|kakashi)\s+/i;
+ const matchPrefix = /^(baby|bby|xan|toru|bot|kakashi)\s+/i;
  if (matchPrefix.test(text)) {
  const query = text.replace(matchPrefix, "").trim();
  if (!query) return;
