f2a1a863c7d4d29198818c00906e8a9147644b90
kakashihatake420000h-cloud <kakashihatake420000h@gmail.com>
2026-02-20 00:07:41 +0600

    Update date2.js

--- modules/commands/date2.js
+++ modules/commands/date2.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "date2",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "𝙈𝙧𝙏𝙤𝙢𝙓𝙭𝙓",
+    credits: "Hridoy",
     description: "Mention your partner",
     commandCategory: "Tag Fun",
     usages: "[@mention]",
